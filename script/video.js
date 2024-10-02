56cc2e7988d2a0cd8b93858e253f39fef16d7f83
Anwar_Hossain <ranakrphone@gmail.com>
2024-10-02 21:24:57 +0600

    categories button added

--- script/video.js
+++ script/video.js
@@ -7,8 +7,18 @@ const loadCatagories = () => {
     .then((data) => displayCatagories(data.categories))
     .catch((err) => console.log(err));
 };
-const displayCatagories = (data) => {
-  console.log(data);
+const displayCatagories = (categories) => {
+  const categoryContainer = document.getElementById("categories");
+
+  categories.forEach((item) => {
+    console.log(item);
+
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+
+    categoryContainer.append(button);
+  });
 };
 
 loadCatagories();
